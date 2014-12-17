2b419d4c6c9c4df8c9b897888b539725546718b1
Liquan Chai <liquanchai@gmail.com>
2014-12-17 03:17:40 +0000

    adjust settings.py to fit new structure

--- src/quiz/settings.py
+++ src/quiz/settings.py
@@ -2,28 +2,28 @@
 Django settings for quiz project.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
+https://docs.djangoproject.com/en/1.6/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
+https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-# auto send email information
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'Animquizwebapp@gmail.com'
-EMAIL_HOST_PASSWORD = '1234567890!!'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+# auto send email information unworke feature
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'Animquizwebapp@gmail.com'
+# EMAIL_HOST_PASSWORD = '1234567890!!'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vj@@$h58exg$%=)wyk$c3imx(@it&(f4!#e6$(h3r3!!_r$zt^'
+SECRET_KEY = '868g+*73=-4=0nwh$!3(43q)4m%#%hy3gxw=m%s=%+$#(_eq4i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -42,19 +42,17 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
-    'joins',
-    'allauth',
-    'allauth.account',
+    'bootstrap_toolkit',
+    'quiz',
+    'usermgt',
+    'south',
 )
-SITE_ID = 1
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -65,7 +63,6 @@ WSGI_APPLICATION = 'quiz.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -75,7 +72,6 @@ DATABASES = {
 }
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -87,59 +83,18 @@ USE_L10N = True
 
 USE_TZ = True
 
+
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-#Specify where the templates are stored
-TEMPLATE_DIRS = (
-    os.path.join(os.path.dirname(BASE_DIR), 'static', 'templates'),
-)
-
-TEMPLATE_CONTEXT_PROCESSORS = ( "django.contrib.auth.context_processors.auth",
-                                "django.core.context_processors.debug",
-                                "django.core.context_processors.i18n",
-                                "django.core.context_processors.media",
-                                "django.core.context_processors.static",
-                                "django.core.context_processors.tz",
-                                "django.contrib.messages.context_processors.messages",
-                                "django.core.context_processors.request",
-                                "allauth.account.context_processors.account",
-                                "allauth.socialaccount.context_processors.socialaccount",
-
-)
-
-AUTHENTICATION_BACKENDS = (
-    # Needed to login by username in Django admin, regardless of `allauth`
-    "django.contrib.auth.backends.ModelBackend",
-
-    # `allauth` specific authentication methods, such as login by e-mail
-    "allauth.account.auth_backends.AuthenticationBackend",
-)
-
-# all-auth config
-LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/'
-
-ACCOUNT_AUTHENTICATION_METHOD = "username_email"
-ACCOUNT_CONFIRM_EMAIL_ON_GET = False #email setup not working
-ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = None
-ACCOUNT_LOGOUT_ON_GET = True
-ACCOUNT_LOGOUT_REDIRECT_URL = LOGIN_URL
-ACCOUNT_SIGNUP_FORM_CLASS = None
-ACCOUNT_SIGNUP_PASSWORD_VERIFICATION = True
-ACCOUNT_UNIQUE_EMAIL = True
-ACCOUNT_USER_MODEL_USERNAME_FIELD = 'username'
-ACCOUNT_USER_MODEL_EMAIL_FIELD = 'email'
-ACCOUNT_USERNAME_MIN_LENGTH = 3
-ACCOUNT_USERNAME_REQUIRED = True
-ACCOUNT_PASSWORD_INPUT_RENDER_VALUE = False
-ACCOUNT_PASSWORD_MIN_LENGTH = 6
-
-
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static', 'root')
-STATICFILES_DIRS = (
-    os.path.join(os.path.dirname(BASE_DIR), 'static', 'static'),
-)
+
+USERMGT_TEMPLATE_DIR = os.path.join(BASE_DIR, 'usermgt/templates')
+
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'template')
+
+TEMPLATE_DIRS = (
+        USERMGT_TEMPLATE_DIR,
+        TEMPLATE_PATH,
+    )
+
+MEDIA_ROOT = BASE_DIR + '/media/'
+MEDIA_URL = '/media/'
