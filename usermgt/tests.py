f5e882ff8da77f5f6cf1e98828af2508797e4342
Liquan Chai <liquanchai@gmail.com>
2014-12-17 06:11:30 +0000

    add unit tests for usermgt application

--- usermgt/tests.py
+++ usermgt/tests.py
@@ -1,5 +1,63 @@
-# coding:utf-8
-
 from django.test import TestCase
+ 
+from django.contrib.auth.models import User
+from usermgt import forms
 
-# Create your tests here.
+
+class UsermgtTests(TestCase):
+    """
+    Test the model and manager used in the default backend.
+    
+    """
+    user_info = {'username': 'liquan',
+                 'password': 'password123',
+                 'email': 'liquan@example.com'}
+    
+    def setUp(self):
+    	pass
+
+    def tearDown(self):
+    	pass
+
+    def test_registration_form(self):
+        """
+        Test that ``RegistrationForm`` enforces username constraints
+        and matching passwords.
+        """
+        # Create a user so we can verify that duplicate usernames aren't
+        # permitted.
+        User.objects.create_user('liquan', 'liquan@example.com', 'secret')
+
+        invalid_data_dicts = [
+            # Non-alphanumeric username.
+            {'data': {'username': 'foo/bar',
+                      'email': 'foo@qq.com',
+                      'password1': 'foo',
+                      'password2': 'foo'},
+            'error': ('username', [u"This value may contain only letters, numbers and @/./+/-/_ characters."])},
+            # Already-existing username.
+            {'data': {'username': 'liquan',
+                      'email': 'liquan@qq.com',
+                      'password1': 'secret',
+                      'password2': 'secret'}
+            'error': ('username', [u"A user with that username already exists."])},
+            # Mismatched passwords.
+            {'data': {'username': 'foo',
+                      'email': 'foo@example.com',
+                      'password1': 'foo',
+                      'password2': 'bar'},
+            'error': ('__all__', [u"The two password fields didn't match."])},
+            ]
+
+        for invalid_dict in invalid_data_dicts:
+            form = forms.RegistrationForm(data=invalid_dict['data'])
+            self.failIf(form.is_valid())
+            self.assertEqual(form.errors[invalid_dict['error'][0]],
+                             invalid_dict['error'][1])
+
+        form = forms.RegistrationForm(data={'username': 'foo',
+                                            'email': 'foo@qq.com',
+                                            'password1': 'foo',
+                                            'password2': 'foo'})
+        self.failUnless(form.is_valid())
+ 
